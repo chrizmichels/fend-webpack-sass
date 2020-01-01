62d065de8834cba0f7bc95d0017e6eb95c9bf75f
chrizmichels <christian@4arge.com>
2020-01-01 19:08:37 +0100

    API fetch implemented

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,37 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  const apiKey = `42b8991393801a8662037ddb8fbd3c5f`;
+  const unit = `metric`;
+  let url = `http://api.openweathermap.org/data/2.5/weather?zip=54578,de&units=${unit}&APPID=${apiKey}`;
+  let apiRes = ``;
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
+  event.preventDefault();
 
-    Client.checkForName(formText)
+  // check what text was put into the form field
+  let formText = document.getElementById("name").value;
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+  Client.checkForName(formText);
+
+  console.log("::: Form Submitted :::");
+  /*   fetch("http://localhost:8081/test")
     .then(res => {
-        return res.json()
+      return res.json();
     })
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+      document.getElementById("results").innerHTML = data.message;
+    }); */
+  console.log(url);
+  fetch(url)
+    .then(res => {
+      return res.json();
     })
+    .then(function(data) {
+      apiRes = `
+      <p> Location: ${data.name} </p>
+      <p>  Temperature: ${data.main.temp} </p>
+      <p>  Feels Like: ${data.main.feels_like} </p>
+        `;
+      document.getElementById("results").innerHTML = apiRes;
+    });
 }
 
-export { handleSubmit }
+export { handleSubmit };
